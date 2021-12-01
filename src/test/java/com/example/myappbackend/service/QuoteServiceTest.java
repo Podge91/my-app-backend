796082cccc0744498fa760c5044eebbc2393c135
Patrick Wilson <pwilson39@qub.ac.uk>
2021-12-01 14:47:40 +0000

    Increased test coverage around factors.

--- src/test/java/com/example/myappbackend/service/QuoteServiceTest.java
+++ src/test/java/com/example/myappbackend/service/QuoteServiceTest.java
@@ -31,10 +31,10 @@ class QuoteServiceTest {
     private String city = "Yemen";
     private String vehicleType = "cabriolet";
     private String engineSize = "1000";
-    private String additionalDrivers = "2";
-    private Boolean commercial = true;
-    private Boolean registeredState = true;
-    private Double currentValue = 15000.00;
+    private String additionalDrivers = "0";
+    private Boolean commercial = false;
+    private Boolean registeredState = false;
+    private Double currentValue = 5000.00;
     private String dateRegistered = "11-11-2021";
 
     Quote quote = new Quote(2L,prefix,firstName,lastName,telephone,addressLine1,addressLine2,city,vehicleType,
@@ -82,7 +82,7 @@ class QuoteServiceTest {
     }
 
     @Test
-    void testFactors1(){
+    void testQuoteAmount1(){
 
         Double testFactor1Result = 371.71;
 
@@ -101,7 +101,7 @@ class QuoteServiceTest {
     }
 
     @Test
-    void testFactors2(){
+    void testQuoteAmount2(){
         Double testFactor2Result = 514.80;
 
         quote.setVehicleType("cabriolet");
@@ -117,6 +117,264 @@ class QuoteServiceTest {
 
         assertEquals(testFactor2Result,calculatedQuote.getQuoteAmount());
 
+    }
+
+    @Test
+    void testFactorCabriolet(){
+        Double testFactorResult = 143.00;
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorCoupe(){
+        Double testFactorResult = 154.00;
+
+        quote.setVehicleType("coupe");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorEstate(){
+        Double testFactorResult = 165.00;
+
+        quote.setVehicleType("estate");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorHatchback(){
+        Double testFactorResult = 176.00;
+
+        quote.setVehicleType("hatchback");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorVehicleTypeOther(){
+        Double testFactorResult = 187.00;
+
+        quote.setVehicleType("Other");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactor1000(){
+        Double testFactorResult = 143.00;
+
+        quote.setEngineSize("1000");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactor1600(){
+        Double testFactorResult = 228.80;
+
+        quote.setEngineSize("1600");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactor2000(){
+        Double testFactorResult = 286.00;
+
+        quote.setEngineSize("2000");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactor2500(){
+        Double testFactorResult = 357.50;
+
+        quote.setEngineSize("2500");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactor3000(){
+        Double testFactorResult = 429.00;
+
+        quote.setEngineSize("3000");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorEngineSizeOther(){
+        Double testFactorResult = 500.50;
+
+        quote.setEngineSize("other");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorVehicleValueLessThan5000(){
+        Double testFactorResult = 143.00;
+
+        quote.setCurrentValue(4999.99);
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorVehicleValue5000(){
+        Double testFactorResult = 143.00;
+
+        quote.setCurrentValue(5000.00);
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorVehicleValueGreaterThan5000(){
+        Double testFactorResult = 171.60;
+
+        quote.setCurrentValue(5000.01);
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorAdditionalDriversLessThan2(){
+        Double testFactorResult = 143.00;
+
+        quote.setAdditionalDrivers("1");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorAdditionalDrivers2(){
+        Double testFactorResult = 156.00;
+
+        quote.setAdditionalDrivers("2");
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorCommercialTrue(){
+        Double testFactorResult = 157.30;
+
+        quote.setCommercial(true);
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorRegisteredStateTrue(){
+        Double testFactorResult = 157.30;
+
+        quote.setRegisteredState(true);
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorCommercialFalse(){
+        Double testFactorResult = 143.00;
+
+        quote.setCommercial(false);
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
+    }
+
+    @Test
+    void testFactorRegisteredStateFalse(){
+        Double testFactorResult = 143.00;
+
+        quote.setRegisteredState(false);
+
+        when(quoteRepository.save(quote)).thenReturn(quote);
+
+        Quote calculatedQuote = quoteService.save(quote);
+
+        assertEquals(testFactorResult,calculatedQuote.getQuoteAmount());
     }
 
 
